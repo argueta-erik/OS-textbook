16058ab7e0f7c3b4a008385986af463b00322d4e
Erik Argueta <argueta.erik@csu.fullerton.edu>
2025-09-17 06:36:24 -0700

    Include notes within fork-example.cpp

--- Ch3-examples/creating-fork/fork-example.cpp
+++ Ch3-examples/creating-fork/fork-example.cpp
@@ -8,10 +8,13 @@ pid_t pid;
 	/* fork a child process */
 	pid = fork();
 	if (pid < 0) { /* error occurres*/
+		// fprintf allows printing in destination and message
+		// stderr is typically unbuffered or line-buffered; ideal for conveying crit error info
 		fprintf(stderr, "Fork Failed");
 		return 1;
 	}
 	else if (pid == 0) { /* child process */
+		// This is the key to outputting correctly in the hw3 assignment.
 		execlp("/bin/ls", "ls", NULL);
 	}
 	else {/* parent process */
